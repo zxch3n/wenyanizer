e09eb8b35d6159487d0580ef1955e23bb705a845
Zixuan Chen <remch183@outlook.com>
2019-12-30 16:00:22 +0800

    Updates

--- precache-manifest.cbc4664c5ef1770aeab8fbefe5ece15f.js
+++ precache-manifest.cbc4664c5ef1770aeab8fbefe5ece15f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "de24414feeb97c2155a8a4215fd741a7",
+    "revision": "b5891572ef24cb81e905d45e86fe7e53",
     "url": "/wenyanizer/index.html"
   },
   {
-    "revision": "5e15bc5b297b02cd803e",
+    "revision": "496500a0fa0839aa7f7e",
     "url": "/wenyanizer/static/css/main.27fd15f6.chunk.css"
   },
   {
-    "revision": "b930fef2d1a21c379075",
-    "url": "/wenyanizer/static/js/2.0578b704.chunk.js"
+    "revision": "b11a09d0241d540582ac",
+    "url": "/wenyanizer/static/js/2.f733204f.chunk.js"
   },
   {
     "revision": "0b1e8647e82c18f1bbe8b4a59fb3fbb8",
-    "url": "/wenyanizer/static/js/2.0578b704.chunk.js.LICENSE"
+    "url": "/wenyanizer/static/js/2.f733204f.chunk.js.LICENSE"
   },
   {
-    "revision": "5e15bc5b297b02cd803e",
-    "url": "/wenyanizer/static/js/main.55ad0771.chunk.js"
+    "revision": "496500a0fa0839aa7f7e",
+    "url": "/wenyanizer/static/js/main.020f9022.chunk.js"
   },
   {
     "revision": "d62ca267bdf6dee0d599",
